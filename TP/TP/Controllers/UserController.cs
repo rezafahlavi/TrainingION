9c75db5c73bd47b88661ee8a6a9c95dc0d051754
Reza Fahlavi <rezafahlavi73@gmail.com>
2024-07-07 20:19:39 +0700

    on progress update banyak field

--- TP/TP/Controllers/UserController.cs
+++ TP/TP/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessService;
+using Microsoft.Ajax.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -56,5 +57,45 @@ namespace TP.Controllers
 
             return View();
         }
+        public ActionResult UserDetail()
+        {
+            UserService service = new UserService();
+            User user = service.GetBy(x => x.UserID == 5).FirstOrDefault();
+
+            return View(user); 
+        }
+
+        [HttpPost]
+        public ActionResult UserDetail(User user) 
+        {
+            UserService service = new UserService();
+            service.Update(user);
+            service.Save();
+
+            return View(user);
+        }
+
+        public ActionResult List()
+        {
+            UserService service = new UserService();
+            IEnumerable<User> user = service.Get().AsEnumerable();
+
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult List(IEnumerable<User> model)
+            
+            
+        {
+            /*
+            for (int i = 0; i < model.Count(); i++) 
+            {
+                User user = model.ElementAt(i);
+            }
+            */
+            
+            return View(model);
+        }
+
     }
 }
