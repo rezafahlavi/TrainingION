5a2397c509de96c322c2500e16190819875a74a7
Reza Fahlavi <rezafahlavi73@gmail.com>
2024-07-13 21:24:15 +0700

    done bedakan halaman view insert edit

--- TP/TP/Controllers/UserController.cs
+++ TP/TP/Controllers/UserController.cs
@@ -28,14 +28,20 @@ namespace TP.Controllers
         [HttpGet]
         public ActionResult Detail(int UserID) 
         {
-            
+            UserService service = new UserService();
+            User user = service.GetBy(x => x.UserID == UserID).FirstOrDefault();
+            ViewBag.Mode = "Edit";
 
+            return View(user);
+        }
+
+        [HttpGet]
+        public ActionResult View(int UserID)
+        {
             UserService service = new UserService();
             User user = service.GetBy(x => x.UserID == UserID).FirstOrDefault();
 
-            //ViewBag.Mode = "View";
-
-            return View(user);
+            return View("Detail", user);
         }
 
         // code Training
