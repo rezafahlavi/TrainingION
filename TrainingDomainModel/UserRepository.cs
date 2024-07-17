f03aa69f7165022e3f9800771275297bd9b92e0f
reza_123 <reza@intersyssolutions.com>
2024-07-17 18:48:14 +0700

    add user repository

---
+++ TrainingDomainModel/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using DomainModelFramework;
+using DomainModelFramework.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Linq;
+
+
+namespace TrainingDomainModel
+{
+    public class UserRepository : DomainRepository<User> 
+    {
+        public UserRepository()
+        {
+            
+
+        }
+        public void ManageUserDetail(User user) 
+        {
+            if (user.UserID > 0)
+            {
+                user.UserDetail.CreatedDate = DateTime.Now;
+                user.UserDetail// tambah
+                this._context.Entry(user.UserDetail).State = EntityState.Modified;
+            }
+            else
+            {
+                this._context.Entry(user.UserDetail).State = EntityState.Added;
+            }
+        }
+    }
+}
