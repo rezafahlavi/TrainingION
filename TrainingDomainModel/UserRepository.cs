6c07818458ae0d476a079b11bfbac4ab521daf89
Reza Fahlavi <rezafahlavi73@gmail.com>
2024-07-20 17:00:23 +0700

    done day 2

--- TrainingDomainModel/UserRepository.cs
+++ TrainingDomainModel/UserRepository.cs
@@ -13,19 +13,26 @@ namespace TrainingDomainModel
     {
         public UserRepository()
         {
-            
-
         }
+        public void DeleteUserDetail(User user)
+        {
+            this._context.Entry(user.UserDetail).State = EntityState.Deleted;
+        }
+
         public void ManageUserDetail(User user) 
         {
             if (user.UserID > 0)
             {
-                user.UserDetail.CreatedDate = DateTime.Now;
-                user.UserDetail// tambah
+                user.UserDetail.ModifiedDate = DateTime.Now;
+                user.UserDetail.ModifiedBy = 0;
                 this._context.Entry(user.UserDetail).State = EntityState.Modified;
             }
             else
             {
+                user.UserDetail.CreatedDate = DateTime.Now;
+                user.UserDetail.CreatedBy = 0;
+                user.UserDetail.ModifiedDate = DateTime.Now;
+                user.UserDetail.ModifiedBy= 0;
                 this._context.Entry(user.UserDetail).State = EntityState.Added;
             }
         }
