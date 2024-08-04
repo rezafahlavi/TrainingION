7f8a0a02ffc4e8a579e66cef0d89ae3699bec160
Reza Fahlavi <rezafahlavi73@gmail.com>
2024-08-04 20:00:12 +0700

    rumah 04 ags 24

---
+++ TP/TP/Scripts/detail.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    var button = $("#btn-insert");
+    button.click(function () {
+        clickButton();
+    });
+});
+
+function clickButton() {
+    $('.loader:first').off('ajaxSuccess');
+    $('.loader:first').off('ajaxFail');
+
+    var data = { // ini ganti semua input di detail?
+        UserName: $("#tb-name").val(),
+        UserDetail: {
+            Phone: $("#tb-phone").val()
+        }
+    };
+
+    $('.loader:first').trigger('loadAjax', [url, data]);
+
+    $('.loader:first').on('ajaxSuccess', function (event, result) {
+        $("#div-result").html(result.Message);
+    });
+    $('.loader:first').on('ajaxFail', function (event, data) {
+        alert(data);
+
+        toggleButton(false);
+    });
+}
+
+
